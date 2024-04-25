d64d289160e221f49cf87944f255acec0bb8de19
maurodelossantos <mad9958@nyu.edu>
2024-04-24 22:07:16 -0400

    Dead symlink fixed. Ignoring the symlink and printing a warning instead.

--- updog/utils/path.py
+++ updog/utils/path.py
@@ -3,6 +3,8 @@ import os
 from math import log2
 from time import ctime
 
+from updog.utils.output import warn
+
 
 def is_valid_subpath(relative_directory, base_directory):
     in_question = os.path.abspath(os.path.join(base_directory, relative_directory))
@@ -31,21 +33,24 @@ def human_readable_file_size(size):
 def process_files(directory_files, base_directory):
     files = []
     for file in directory_files:
-        if file.is_dir():
-            size = '--'
-            size_sort = -1
+        if os.path.exists(file.path):
+            if file.is_dir():
+                size = '--'
+                size_sort = -1
+            else:
+                size = human_readable_file_size(file.stat().st_size)
+                size_sort = file.stat().st_size
+            files.append({
+                'name': file.name,
+                'is_dir': file.is_dir(),
+                'rel_path': get_relative_path(file.path, base_directory),
+                'size': size,
+                'size_sort': size_sort,
+                'last_modified': ctime(file.stat().st_mtime),
+                'last_modified_sort': file.stat().st_mtime
+            })
         else:
-            size = human_readable_file_size(file.stat().st_size)
-            size_sort = file.stat().st_size
-        files.append({
-            'name': file.name,
-            'is_dir': file.is_dir(),
-            'rel_path': get_relative_path(file.path, base_directory),
-            'size': size,
-            'size_sort': size_sort,
-            'last_modified': ctime(file.stat().st_mtime),
-            'last_modified_sort': file.stat().st_mtime
-        })
+            warn(f"File {file.path} does not exist!")
     return files
 
 
